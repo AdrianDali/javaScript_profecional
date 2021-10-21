9f2f29483c3e64bf398b33c79d18cfe720741c7a
AdrianDali <adrianhernandez@aragon.unam.mx>
2021-10-20 23:57:35 -0500

    El primer plugin apuntes2

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,3 +1,4 @@
+//creamos el objeto media
 function MediaPlayer(config){
   this.media = config.el
   this.plugins = config.plugins || [];
@@ -10,15 +11,17 @@ MediaPlayer.prototype._initPlugins = function(){
       plugin.run(this)
     })
 }
-
+/**Esta es la funcion que inicia la ejecucion de nuestro video */
 MediaPlayer.prototype.play = function(){
   this.media.play();
 }
 
+/**Esta es la funcion que le pone pausa a nuestro video */
 MediaPlayer.prototype.pause = function(){
     this.media.pause();
 }
 
+/**Esta funcion Pone pausa o play al video */
 MediaPlayer.prototype.togglePlay = function(){
     if(this.media.paused){
         this.play();
@@ -27,12 +30,13 @@ MediaPlayer.prototype.togglePlay = function(){
         this.pause();
     }
 }
-
+/**Esta es la funcion que le pone mute a nuestro video */
 MediaPlayer.prototype.mute = function(){
   this.media.muted = true
 }
+/**Esta es la funcion que le quita el muteo a nuestra funcion */
 MediaPlayer.prototype.unmute = function(){
   this.media.muted = false;
 }
-
+//Exportamos la funcion 
 export default MediaPlayer
--- assets/index.js
+++ assets/index.js
@@ -7,13 +7,19 @@ import AutoPlay from "./plugins/AutoPlay.js"
  * SELECTORES: son usados para apuntar al elemento HTML que quieres estilizar
  * */
 const video = document.querySelector('video')
-
+/**Este metodo lo que hace es enviar el elemento y el plugin para ser inicializados
+ * en Mediaplayer y el video inicie automaticamente
+ */
 const player = new MediaPlayer({el: video,plugins: [new AutoPlay()]});
 
+/** Enlace al botton de play*/
 const button = document.querySelector('#playButton')
+/**Cuando se presione el boton se ejecutara la funcion de togglePlay */
 button.onclick = () => player.togglePlay();
 
+/**Enlace al boton de mute */
 const muteButton = document.querySelector('#muteButton');
+/**Cuando se presione el boton se ejecutara una arrow funcion */
 muteButton.onclick = () => {
   if (player.media.muted) {
     player.unmute();
